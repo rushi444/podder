0dc5fa9ee9d27f61d4d5e98df71c39891262303f
Rushi <rushi44@gmail.com>
2020-12-22 08:09:23 -0800

    lib

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import { ApolloProvider } from '@apollo/client'
+import { ChakraProvider } from '@chakra-ui/react'
+
+import { App } from './components/App'
+import { client } from './lib/apolloClient'
+import { theme } from './lib/chakraTheme'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <ChakraProvider theme={theme} resetCSS={true}>
+        <App />
+      </ChakraProvider>
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
