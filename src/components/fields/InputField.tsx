f1b0ef1ebb2f6d606209c3ebac512958936c16d7
Rushi <rushi44@gmail.com>
2020-12-24 20:43:55 -0800

    feat(register): rules + passwordMatch

--- src/components/fields/InputField.tsx
+++ src/components/fields/InputField.tsx
@@ -14,16 +14,19 @@ type Props = {
   name: string
   control: Control
   rules?: any
+  ref?: any
+  type?: string
 }
 
 export const InputField = (props: Props) => {
+  const { type = 'text' } = props
   const { field, meta } = useController(props)
   const { errorMessage, hasError } = useMetaError(meta)
   return (
     <Box>
       <FormControl isInvalid={hasError}>
         <FormLabel>{props.name}</FormLabel>
-        <Input {...field} />
+        <Input {...field} type={type} />
         <FormErrorMessage>{errorMessage}</FormErrorMessage>
       </FormControl>
     </Box>
