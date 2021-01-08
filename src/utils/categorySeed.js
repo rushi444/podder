353bfbe02263ce62888fae8ea2f4218635d74e6e
Rushi <rushi44@gmail.com>
2021-01-07 20:50:23 -0800

    create category seed script :bento:

---
+++ src/utils/categorySeed.js
@@ -0,0 +1,58 @@
+var axios = require('axios')
+const { gql } = require('@apollo/client')
+const { print } = require('graphql')
+
+const categories = [
+  'History',
+  'Educational',
+  'Religious',
+  'Audio Dramas',
+  'Sports',
+  'Comedy',
+  'Society & Culture',
+  'Feminist',
+  'Health',
+  'Advice & Self-Help',
+  'Business',
+  'News & Politics',
+  'Personal Journal',
+  'Interview Cast',
+  'Environment/Science',
+  'Technology',
+  'Crime',
+  'Kids & Families',
+  'Law',
+  'Pop Culture',
+  'Philosophy',
+  'Games & Hobbies',
+]
+
+const CREATE_CATEGORY = gql`
+  mutation CreateCategory($input: batchCategoriesInput) {
+    createCategory(input: $input) {
+      id
+      name
+    }
+  }
+`
+
+const seedCategories = async categories => {
+  return await categories.forEach(async category => {
+    try {
+      const { data } = await axios.post('http://localhost:4000/dev/graphql', {
+        query: print(CREATE_CATEGORY),
+        variables: {
+          input: {
+            name: category,
+          },
+        },
+      })
+      console.log(data)
+      return data
+    } catch (err) {
+      console.log('ERR', err)
+    }
+  })
+}
+
+seedCategories(categories)
---
+++ src/utils/staticCategories.ts
@@ -0,0 +1,90 @@
+export const categories = [
+  {
+    id: '8d4ebe7a-da6e-4082-b818-dda0e3049e9f',
+    name: 'History',
+  },
+  {
+    id: '5872646c-8cd3-4026-86ac-ead83e9e5d97',
+    name: 'Educational',
+  },
+  {
+    id: 'f8f795e1-f0d8-45a9-8118-754a80a35b7b',
+    name: 'Religious',
+  },
+  {
+    id: 'd8d9ab17-cf5b-42ac-a166-072ca8cb5114',
+    name: 'Audio Dramas',
+  },
+  {
+    id: '063f0e62-db26-48e3-b38f-3c01b303689f',
+    name: 'Sports',
+  },
+  {
+    id: '32966ed6-22d3-4734-9064-edcef3c747b0',
+    name: 'Comedy',
+  },
+  {
+    id: 'b233be9a-5d21-4366-81c2-49f7fd08194a',
+    name: 'Society & Culture',
+  },
+  {
+    id: '5c131017-e272-4a36-a008-9046fc0c16c2',
+    name: 'Feminist',
+  },
+  {
+    id: 'afd9e270-f0b2-40a9-8de6-492ba1fb754f',
+    name: 'Health',
+  },
+  {
+    id: '9ab38608-7aa7-4c0a-a72a-a639fbcdfb17',
+    name: 'Advice and Self-Help',
+  },
+  {
+    id: 'c4c06d16-ebbe-428f-bce2-151eca6c14ce',
+    name: 'Business',
+  },
+  {
+    id: '37674c7b-062e-4547-911e-1b1fac19328c',
+    name: 'News & Politics',
+  },
+  {
+    id: '93c38c21-e7be-487d-bc3e-8873fbb13b5c',
+    name: 'Interview Cast',
+  },
+  {
+    id: '0c79fa22-b077-4b60-a4a9-db9e07b70325',
+    name: 'Crime',
+  },
+  {
+    id: 'ac737933-12c0-4942-8714-10e4351bb01b',
+    name: 'Games & Hobbies',
+  },
+  {
+    id: '9aa01ea5-4d26-453e-96ad-e3f97c3effc2',
+    name: 'Technology',
+  },
+  {
+    id: '8fa18acb-ffcc-4311-b9f0-13e0e2e5f0c6',
+    name: 'Philosophy',
+  },
+  {
+    id: '242d0cb1-6622-43bd-a41c-de92df5fb6c0',
+    name: 'Kids & Families',
+  },
+  {
+    id: 'e2a9cee3-70c2-4ee7-aa12-99e242cb03f3',
+    name: 'Personal Journal',
+  },
+  {
+    id: '313dfbf4-692c-47c0-919a-fb96f704e861',
+    name: 'Environment/Science',
+  },
+  {
+    id: '956100b5-010b-4049-85cb-3fbb051d06fd',
+    name: 'Pop Culture',
+  },
+  {
+    id: '98bed92c-a3a0-4d70-a5a5-18a6f74002d3',
+    name: 'Law',
+  },
+]
